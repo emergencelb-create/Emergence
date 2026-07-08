1e42d15a3805fbf04023067675fb6841b81b2f79
emergencelb-create <emergencelb@gmail.com>
2026-07-08 08:58:38 +0200

    Add files via upload

---
+++ produits.js
@@ -0,0 +1,103 @@
+document.write(`
+
+<div class="carte">
+
+<h2>🛍️ À découvrir également</h2>
+
+<div class="produit">
+
+<img src="images/10routines.jpg" width="120">
+
+<div>
+
+<h3>📘 10 routines neuroscientifiques</h3>
+
+<p>
+10 routines simples pour aider votre système nerveux à retrouver le calme.
+</p>
+
+<a class="bouton" href="pdfvente10routines.html">
+
+Découvrir
+
+</a>
+
+</div>
+
+</div>
+
+<hr>
+
+<div class="produit">
+
+<img src="images/fondamentaux.jpg" width="120">
+
+<div>
+
+<h3>🧠 Les Fondamentaux des Neurosciences</h3>
+
+<p>
+Comprendre enfin comment fonctionne votre cerveau.
+</p>
+
+<a class="bouton" href="pdfventefondamentaux.html">
+
+Découvrir
+
+</a>
+
+</div>
+
+</div>
+
+<hr>
+
+<div class="produit">
+
+<img src="images/meditation-the.jpg" width="120">
+
+<div>
+
+<h3>🍵 Méditation du thé</h3>
+
+<p>
+Retrouver un état de sécurité intérieure.
+</p>
+
+<a class="bouton" href="meditation-the.html">
+
+Écouter
+
+</a>
+
+</div>
+
+</div>
+
+<hr>
+
+<div class="produit">
+
+<img src="images/visualisation.jpg" width="120">
+
+<div>
+
+<h3>✨ Visualisation de votre meilleure vie</h3>
+
+<p>
+Projetez votre cerveau vers un futur plus serein.
+</p>
+
+<a class="bouton" href="meditation-visualisation.html">
+
+Découvrir
+
+</a>
+
+</div>
+
+</div>
+
+</div>
+
+`);
